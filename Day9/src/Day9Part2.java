0c8748ce3e0dc62ab0b941e31a0b00e55911b062
soradotwav <soradesignz@gmail.com>
2023-12-08 22:55:06 -0800

    Finished Day 9

---
+++ Day9/src/Day9Part2.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day9Part2 {
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(new File("input.txt"))) {
+            int sum = 0;
+
+            while(scanner.hasNextLine()) {
+                int[] curr = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+                sum += getPreviousInSequence(curr);
+            }
+
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Recursively calculates the previous number in a sequence based on the input array.
+     *
+     * @param input The input array representing a sequence.
+     * @return The previous number in the sequence.
+     */
+    private static int getPreviousInSequence(int[] input) {
+        int count = 0;
+        for(int i : input) if(i == 0) count++;
+        if(count == input.length) return 0;
+
+        int[] dif = new int[input.length - 1];
+
+        for(int i = 0; i < dif.length; i++) {
+            dif[i] = input[i + 1] - input[i];
+        }
+        return input[0] - getPreviousInSequence(dif);
+    }
+}
